7c71b48551da25f75278492ba2b874e0f7b25ee8
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-09-15 23:24:56 -0500

    canvas properties

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-console.log(maps);
+const $ = (selector) => document.querySelector(selector);
+const canvas = $("#game");
+const game = canvas.getContext("2d");
+
+window.addEventListener('load', startGame);
+
+function startGame() {
+  // game.fillRect(50, 50, 100, 100);
+  game.font = '30px Arial';
+  game.fillStyle = 'red';
+  game.textAlign = 'right';
+  game.fillText('Hello World', 100, 100);
+  // game.clearRect(50, 50, 100, 100);
+  // game.clearRect(0, 0, 50, 50);
+}
